93eb4fac11ed8260139da2235048869913742cc1
ivan-alfonso <ivandaalf@hotmail.com>
2017-04-25 18:56:50 -0500

    Prueba index

--- laboratorio/urls.py
+++ laboratorio/urls.py
@@ -17,7 +17,9 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
+from . import views
 
 urlpatterns = [
+	url(r'^$', views.index, name='index'),
     url(r'^admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
