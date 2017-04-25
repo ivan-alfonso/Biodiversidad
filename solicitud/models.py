381a6c13154b153c046d16ea83bec13368d2d285
ivan-alfonso <ivandaalf@hotmail.com>
2017-04-25 18:17:11 -0500

    Cambio de prueba

--- solicitud/models.py
+++ solicitud/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Solicitud(models.Model):
-    texto = models.CharField(max_length=255, blank=True, null=True)
+    texto = models.CharField(max_length=255, blank=True, null=True)	
     cientifico = models.CharField(max_length=30, blank=True, null=True)
